65586a999dc3606c9170e5da9de94313cec40e44
Wayne Kenneth Seymour III <WayneSeymour@iheartmedia.com>
2017-08-18 10:22:11 -0400

    Cleaned.

--- reduce.py
+++ reduce.py
@@ -2,3 +2,21 @@ def add(a, b):
     return a + b
 
 print reduce(add, [0, 1, 2, 3, 4])
+
+sentences = ['Mary read a story to Sam and Isla.',
+             'Isla cuddled Sam.',
+             'Sam chortled.']
+
+def unfunc(xs):
+    sam_count = 0
+    for sentence in xs:
+        sam_count += sentence.count('Sam')
+
+    print sam_count
+
+
+def func(xs):
+    return reduce(lambda a, b: a + b.count('Sam'), xs, 0)
+
+unfunc(sentences)
+print func(sentences)
